71015b9a3200595496f769bca0c78c2c59af545b
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-12-22 21:16:27 -0300

    chore: add `@zephyr/tokens` library in the `@zephyr/ui`

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig, Options } from "tsup";
+import { defineConfig, Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
   entry: {
-    index: "src/index.tsx",
+    index: 'src/index.tsx',
   },
   banner: {
     js: "'use client'",
   },
   clean: true,
-  format: ["cjs", "esm"],
-  external: ["react", "react-native"],
+  format: ['cjs', 'esm'],
+  external: ['react', 'react-native', '@zephyr/tokens'],
   dts: true,
   ...options,
 }));
