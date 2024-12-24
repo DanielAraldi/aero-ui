68dece2a4e1c41654490243e0ca243f82f244e55
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-12-24 20:17:51 -0300

    style: set default `height` to `FontWeights` and `LetterSpacings`

--- apps/docs/components/tokens/LetterSpacings/styles.ts
+++ apps/docs/components/tokens/LetterSpacings/styles.ts
@@ -6,6 +6,7 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
 
     width: '100%',
+    height: spacings[12],
 
     justifyContent: 'space-between',
     alignItems: 'center',
