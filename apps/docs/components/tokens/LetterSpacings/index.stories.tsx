32d3ffe1159e1c832e01aa93c15aa65f08b206dd
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-12-24 20:01:42 -0300

    feat: add `LetterSpacings` in storybook documentation

---
+++ apps/docs/components/tokens/LetterSpacings/index.stories.tsx
@@ -0,0 +1,31 @@
+import { ScrollView, Text, View } from 'react-native';
+import { StoryObj, Meta } from '@storybook/react';
+import { letterSpacings } from '@zephyr/tokens';
+
+import { globalStyles } from '../../styles';
+import { styles } from './styles';
+
+const meta: Meta = {
+  title: 'Tokens',
+  component: () => (
+    <ScrollView
+      style={globalStyles.container}
+      contentContainerStyle={globalStyles.spaceBetweenContent}
+      showsVerticalScrollIndicator={false}
+    >
+      {Object.entries(letterSpacings).map(([key, letterSpacing]) => (
+        <View key={key} style={styles.content}>
+          <Text style={[globalStyles.text, { letterSpacing }]}>{key}</Text>
+
+          <Text style={[globalStyles.text, { letterSpacing }]}>
+            {letterSpacing}px
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
+  ),
+};
+
+export default meta;
+
+export const LetterSpacings: StoryObj = {};
---
+++ apps/docs/components/tokens/LetterSpacings/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import { colors, spacings } from '@zephyr/tokens';
+
+export const styles = StyleSheet.create({
+  content: {
+    flexDirection: 'row',
+
+    width: '100%',
+
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    padding: spacings[2],
+
+    backgroundColor: colors.blue[200],
+  },
+});
