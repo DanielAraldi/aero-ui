d3f6785b4f833e3f969883e62c11ea845961f95a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-12-24 14:46:30 -0300

    feat: add `Opacities` in storybook documentation

---
+++ apps/docs/components/tokens/Opacities/index.stories.tsx
@@ -0,0 +1,21 @@
+import { ScrollView, Text, View } from 'react-native';
+import { StoryObj, Meta } from '@storybook/react';
+import { opacities } from '@zephyr/tokens';
+import { styles } from './styles';
+
+const meta: Meta = {
+  title: 'Tokens',
+  component: () => (
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      {Object.entries(opacities).map(([key, value]) => (
+        <View key={key} style={[styles.content, { opacity: value }]}>
+          <Text style={styles.text}>opacity/{key}</Text>
+        </View>
+      ))}
+    </ScrollView>
+  ),
+};
+
+export default meta;
+
+export const Opacities: StoryObj = {};
