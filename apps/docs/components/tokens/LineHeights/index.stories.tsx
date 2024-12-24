a073c95964f75791e32eab863506d3c1deed5502
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-12-24 20:06:51 -0300

    feat: add `LineHeights` in storybook documentation

---
+++ apps/docs/components/tokens/LineHeights/index.stories.tsx
@@ -0,0 +1,31 @@
+import { ScrollView, Text, View } from 'react-native';
+import { StoryObj, Meta } from '@storybook/react';
+import { lineHeights } from '@zephyr/tokens';
+
+import { globalStyles } from '../../styles';
+import { styles } from './styles';
+
+const meta: Meta = {
+  title: 'Tokens',
+  component: () => (
+    <ScrollView
+      style={globalStyles.container}
+      contentContainerStyle={globalStyles.spaceBetweenContent}
+      showsVerticalScrollIndicator={false}
+    >
+      {Object.entries(lineHeights).map(([key, lineHeight]) => (
+        <View key={key} style={styles.content}>
+          <Text style={[globalStyles.text, { lineHeight }]}>{key}</Text>
+
+          <Text style={[globalStyles.text, { lineHeight }]}>
+            {lineHeight}px
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
+  ),
+};
+
+export default meta;
+
+export const LineHeights: StoryObj = {};
