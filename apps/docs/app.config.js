9dd5ffa6770f00d5c06fb6fb5267422395be5f69
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-01-22 22:16:19 -0300

    chore: remove unnecessary fonts libraries in storybook documentation

--- apps/docs/app.config.js
+++ apps/docs/app.config.js
@@ -2,7 +2,6 @@ export default ({ config }) => ({
   ...config,
   name: 'Aero UI',
   slug: 'aero-ui',
-  plugins: ['expo-font'],
   extra: {
     storybookEnabled: process.env.STORYBOOK_ENABLED,
   },
