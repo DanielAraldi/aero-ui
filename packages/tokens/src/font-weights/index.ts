8fcf9189bc74ddff3efb87953a687502729b6880
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-01-23 21:31:41 -0300

    feat: add `thin`, `extralight`, `extrabold` and `black` weight in the `fontWeights`

--- packages/tokens/src/font-weights/index.ts
+++ packages/tokens/src/font-weights/index.ts
@@ -1,9 +1,13 @@
 export const fontWeights = {
+  thin: '100',
+  extralight: '200',
   light: '300',
   regular: '400',
   medium: '500',
   semibold: '600',
   bold: '700',
+  extrabold: '800',
+  black: '900',
 } as const;
 
 export type FontWeightsObjectType = typeof fontWeights;
