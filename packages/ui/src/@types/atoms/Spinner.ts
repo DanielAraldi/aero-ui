0cc1e946ce8ccfeb7371f6c5b2c9a6dfa6291328
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-01-25 19:59:33 -0300

    feat: add `overlayColor` in the `SpinnerProps` interface

--- packages/ui/src/@types/atoms/Spinner.ts
+++ packages/ui/src/@types/atoms/Spinner.ts
@@ -4,5 +4,6 @@ export type SpinnerColorType = 'small' | 'normal' | 'large';
 
 export interface SpinnerProps {
   size?: SpinnerColorType;
-  color?: ColorValue | string;
+  color?: ColorValue;
+  overlayColor?: ColorValue;
 }
