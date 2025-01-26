763435fad0c5f75c4c55a0a78b9bced150d016e7
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-01-26 14:25:59 -0300

    feat: add `variant`, `overlayColor`, `startBy`, `duration`, and `easing` properties for SpinnerProps

--- packages/ui/src/@types/atoms/Spinner.ts
+++ packages/ui/src/@types/atoms/Spinner.ts
@@ -1,9 +1,52 @@
-import { ColorValue } from 'react-native';
+import { ColorValue, EasingFunction } from 'react-native';
 
-export type SpinnerColorType = 'small' | 'normal' | 'large';
+export type SpinnerVariantType = 'unique' | 'double' | 'half';
+
+export type SpinnerSizeType = 'small' | 'normal' | 'large';
+
+export type SpinnerStartByType = 'bottom' | 'left' | 'right' | 'top';
 
 export interface SpinnerProps {
-  size?: SpinnerColorType;
+  size?: SpinnerSizeType;
   color?: ColorValue;
+
+  /**
+   * @description This property is used to determine the scroll variants of the
+   * spinner.
+   *
+   * @default 'unique'
+   */
+  variant?: SpinnerVariantType;
+
+  /**
+   * @description This property is used to control the color of the spinner
+   * overlay.
+   *
+   * @default 'transparent'
+   */
   overlayColor?: ColorValue;
+
+  /**
+   * @description This property is used to determine the local where the
+   * spinner starts to rotate.
+   *
+   * @default 'left'
+   */
+  startBy?: SpinnerStartByType;
+
+  /**
+   * @description This property is used to control the duration of the
+   * rotation animation.
+   *
+   * @default 1000
+   */
+  duration?: number;
+
+  /**
+   * @description This property is used to control the easing function of the
+   * rotation animation.
+   *
+   * @default Easing.linear
+   */
+  easing?: EasingFunction;
 }
