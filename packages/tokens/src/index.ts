01b27aacd1517bd3a8aad9db96866a94d315d35c
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-02-02 13:39:15 -0300

    fix: add manual import types in intermediary index file

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,10 +1,46 @@
-export * from './border-radius';
-export * from './border-widths';
-export * from './colors';
-export * from './font-sizes';
-export * from './font-weights';
-export * from './letter-spacings';
-export * from './line-heights';
-export * from './opacities';
-export * from './spacings';
-export * from './z-indexes';
+export {
+  type BorderRadiusKeysType,
+  type BorderRadiusObjectType,
+  borderRadius,
+} from './border-radius';
+export {
+  type BorderWidthsKeysType,
+  type BorderWidthsObjectType,
+  borderWidths,
+} from './border-widths';
+export { type ColorsKeysType, type ColorsObjectType, colors } from './colors';
+export {
+  type FontSizesKeysType,
+  type FontSizesObjectType,
+  fontSizes,
+} from './font-sizes';
+export {
+  type FontWeightsKeysType,
+  type FontWeightsObjectType,
+  fontWeights,
+} from './font-weights';
+export {
+  type LetterSpacingsKeysType,
+  type LetterSpacingsObjectType,
+  letterSpacings,
+} from './letter-spacings';
+export {
+  type LineHeightsKeysType,
+  type LineHeightsObjectType,
+  lineHeights,
+} from './line-heights';
+export {
+  type OpacitiesKeysType,
+  type OpacitiesObjectType,
+  opacities,
+} from './opacities';
+export {
+  type SpacingsKeysType,
+  type SpacingsObjectType,
+  spacings,
+} from './spacings';
+export {
+  type ZIndexesKeysType,
+  type ZIndexesObjectType,
+  zIndexes,
+} from './z-indexes';
