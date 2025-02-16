b894db03ec085790c63cf052618bc357610ec313
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-02-16 15:17:51 -0300

    docs: add JSDoc to the `TextProps` interface

--- packages/ui/src/@types/atoms/Text.ts
+++ packages/ui/src/@types/atoms/Text.ts
@@ -2,5 +2,12 @@ import { TextProps as ReactNativeTextProps } from 'react-native';
 import { FontSizesKeysType } from '@aero-ui/tokens';
 
 export interface TextProps extends ReactNativeTextProps {
+  /**
+   * @description This property is used to determine the variants of the text.
+   *
+   * @default 'base'
+   *
+   * @since 1.2.0
+   */
   variant?: FontSizesKeysType;
 }
