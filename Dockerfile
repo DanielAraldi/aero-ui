5df60546627026063d7c21828526f844ed17d9a8
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-05 09:57:57 -0300

    chore: upgrade in the react native version

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ COPY yarn.lock .
 COPY ./packages ./packages
 
 RUN yarn install --frozen-lockfile
-RUN cd ./packages/ui && yarn add react@18.2.0 react-native@0.73.1 -D && cd ../..
+RUN cd ./packages/ui && yarn add react@18.2.0 react-native@0.74.5 -D && cd ../..
 
 RUN yarn build
 RUN yarn test
--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -172,7 +172,7 @@ const config: Config = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native)',
+    'node_modules/(?!(jest-)?react-native|@react-native|@testing-library/react-native)',
   ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
