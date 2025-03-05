304656ddfef7418afc97e6987de475830a5189e9
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-05 08:58:32 -0300

    fix: add react netive steup jest in the `jest.setup.js`

--- packages/ui/jest.setup.js
+++ packages/ui/jest.setup.js
@@ -1,2 +1,4 @@
+import 'react-native/jest/setup';
+
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
