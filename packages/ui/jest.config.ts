849d1897d099fbca04866e8eeceb8ad967d209ef
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-05 09:06:57 -0300

    fix: add `moduleNameMapper` and others mocks in the jest setup

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -81,7 +81,9 @@ const config: Config = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^react-native$': 'react-native',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
--- packages/ui/jest.setup.js
+++ packages/ui/jest.setup.js
@@ -1,4 +1,3 @@
-import 'react-native/jest/setup';
-
-// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+jest.mock('react-native/Libraries/LogBox/LogBox');
