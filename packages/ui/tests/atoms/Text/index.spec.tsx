ee6cb820d779a9cb7f33203bad703fdbcb13e897
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-06 14:24:35 -0300

    test: should render `Text` component with `xs` font configuration for text

--- packages/ui/tests/atoms/Text/index.spec.tsx
+++ packages/ui/tests/atoms/Text/index.spec.tsx
@@ -1,4 +1,10 @@
 import { render, screen } from '@testing-library/react-native';
+import {
+  fontSizes,
+  fontWeights,
+  letterSpacings,
+  lineHeights,
+} from '@aero-ui/tokens';
 
 import { Text } from '../../../';
 
@@ -11,5 +17,15 @@ describe('<Text />', () => {
   it("Should render Text component with 'Aero UI' text", () => {
     render(<Text>Aero UI</Text>);
     expect(screen.getByText('Aero UI')).toBeOnTheScreen();
+  });
+
+  it('Should render Text component with xs font configuration for text', () => {
+    render(<Text variant='xs'>xs</Text>);
+    expect(screen.getByTestId('text')).toHaveStyle({
+      fontSize: fontSizes.xs,
+      fontWeight: fontWeights.regular,
+      lineHeight: lineHeights.xs,
+      letterSpacing: letterSpacings.normal,
+    });
   });
 });
