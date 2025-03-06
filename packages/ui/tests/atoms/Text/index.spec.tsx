3489d253089263cea827252e8340a7faed6d44c8
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-06 14:14:45 -0300

    test: should render `Text` component with "Aero UI" text

--- packages/ui/tests/atoms/Text/index.spec.tsx
+++ packages/ui/tests/atoms/Text/index.spec.tsx
@@ -3,8 +3,13 @@ import { render, screen } from '@testing-library/react-native';
 import { Text } from '../../../';
 
 describe('<Text />', () => {
-  test('Should render Text component', () => {
+  it('Should render Text component', () => {
     render(<Text />);
     expect(screen.getByTestId('text')).toBeOnTheScreen();
+  });
+
+  it("Should render Text component with 'Aero UI' text", () => {
+    render(<Text>Aero UI</Text>);
+    expect(screen.getByText('Aero UI')).toBeOnTheScreen();
   });
 });
