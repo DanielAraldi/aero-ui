92467bff1ad0cd1b5bad7876c6e448450b653902
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-06 14:29:07 -0300

    test: should render `Text` component with `6xl` font configuration for text

--- packages/ui/tests/atoms/Text/index.spec.tsx
+++ packages/ui/tests/atoms/Text/index.spec.tsx
@@ -108,4 +108,14 @@ describe('<Text />', () => {
       letterSpacing: letterSpacings.normal,
     });
   });
+
+  it('Should render Text component with 6xl font configuration for text', () => {
+    render(<Text variant='6xl'>6xl</Text>);
+    expect(screen.getByTestId('text')).toHaveStyle({
+      fontSize: fontSizes['6xl'],
+      fontWeight: fontWeights.regular,
+      lineHeight: lineHeights['6xl'],
+      letterSpacing: letterSpacings.normal,
+    });
+  });
 });
