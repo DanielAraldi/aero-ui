973b40a3778ff007866db8b66554f70cf6d6e566
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-06 14:28:04 -0300

    test: should render `Text` component with `3xl` font configuration for text

--- packages/ui/tests/atoms/Text/index.spec.tsx
+++ packages/ui/tests/atoms/Text/index.spec.tsx
@@ -78,4 +78,14 @@ describe('<Text />', () => {
       letterSpacing: letterSpacings.normal,
     });
   });
+
+  it('Should render Text component with 3xl font configuration for text', () => {
+    render(<Text variant='3xl'>3xl</Text>);
+    expect(screen.getByTestId('text')).toHaveStyle({
+      fontSize: fontSizes['3xl'],
+      fontWeight: fontWeights.regular,
+      lineHeight: lineHeights['3xl'],
+      letterSpacing: letterSpacings.normal,
+    });
+  });
 });
