76e096996520546fdb74910046ffe2399b0b04fc
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-07 14:47:02 -0300

    test(ios): should render `Spinner` component with half variant, top startBy and colors provided

--- packages/ui/tests/atoms/Spinner/index.spec.tsx
+++ packages/ui/tests/atoms/Spinner/index.spec.tsx
@@ -447,6 +447,34 @@ describe('<Spinner />', () => {
       expect(screen.getByTestId('spinner')).toHaveStyle(startByStyles);
     });
 
+    it('Should render Spinner component with half variant, top startBy and colors provided', () => {
+      mockPlatform('ios');
+
+      const { spinnerStartByStylesStub } = makeSut();
+
+      const color = faker.color.rgb();
+      const overlayColor = faker.color.rgb();
+
+      render(
+        <Spinner
+          variant='half'
+          startBy='top'
+          color={color}
+          overlayColor={overlayColor}
+          useNativeDriver={false}
+        />
+      );
+
+      const startByStyles = spinnerStartByStylesStub({
+        variant: 'half',
+        startBy: 'top',
+        color,
+        overlayColor,
+      });
+
+      expect(screen.getByTestId('spinner')).toHaveStyle(startByStyles);
+    });
+
     it('Should take a snapshot of the Spinner component', () => {
       mockPlatform('ios');
 
