b004b843fbf52a85e5365101452408d8f6a4df27
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-07 14:02:52 -0300

    test(ios): should render `Spinner` component with `half` variant

--- packages/ui/tests/atoms/Spinner/index.spec.tsx
+++ packages/ui/tests/atoms/Spinner/index.spec.tsx
@@ -194,6 +194,21 @@ describe('<Spinner />', () => {
       expect(screen.getByTestId('spinner')).toHaveStyle(startByStyles);
     });
 
+    it('Should render Spinner component with half variant', () => {
+      mockPlatform('ios');
+
+      const { spinnerStartByStylesStub } = makeSut();
+
+      render(<Spinner variant='half' useNativeDriver={false} />);
+      const startByStyles = spinnerStartByStylesStub({
+        variant: 'half',
+        startBy: 'left',
+        color: colors.black[100],
+        overlayColor: 'transparent',
+      });
+      expect(screen.getByTestId('spinner')).toHaveStyle(startByStyles);
+    });
+
     it('Should take a snapshot of the Spinner component', () => {
       mockPlatform('ios');
 
