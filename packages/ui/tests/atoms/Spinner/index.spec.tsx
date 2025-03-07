dd0a1bde2cf3fca40ae723a8c8cbfa254f8e8403
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-07 13:57:28 -0300

    test(ios): should render `Spinner` component with `unique` variant

--- packages/ui/tests/atoms/Spinner/index.spec.tsx
+++ packages/ui/tests/atoms/Spinner/index.spec.tsx
@@ -164,6 +164,21 @@ describe('<Spinner />', () => {
       );
     });
 
+    it('Should render Spinner component with unique variant', () => {
+      mockPlatform('ios');
+
+      const { spinnerStartByStylesStub } = makeSut();
+
+      render(<Spinner variant='unique' useNativeDriver={false} />);
+      const startByStyles = spinnerStartByStylesStub({
+        variant: 'unique',
+        startBy: 'left',
+        color: colors.black[100],
+        overlayColor: 'transparent',
+      });
+      expect(screen.getByTestId('spinner')).toHaveStyle(startByStyles);
+    });
+
     it('Should take a snapshot of the Spinner component', () => {
       mockPlatform('ios');
 
