b051f4c28936c85d5f523baac277c799c56a94bb
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-07 14:33:37 -0300

    test(ios): should render `Spinner` component with double variant, bottom startBy and colors provided

--- packages/ui/tests/atoms/Spinner/index.spec.tsx
+++ packages/ui/tests/atoms/Spinner/index.spec.tsx
@@ -363,6 +363,34 @@ describe('<Spinner />', () => {
       expect(screen.getByTestId('spinner')).toHaveStyle(startByStyles);
     });
 
+    it('Should render Spinner component with double variant, bottom startBy and colors provided', () => {
+      mockPlatform('ios');
+
+      const { spinnerStartByStylesStub } = makeSut();
+
+      const color = faker.color.rgb();
+      const overlayColor = faker.color.rgb();
+
+      render(
+        <Spinner
+          variant='double'
+          startBy='bottom'
+          color={color}
+          overlayColor={overlayColor}
+          useNativeDriver={false}
+        />
+      );
+
+      const startByStyles = spinnerStartByStylesStub({
+        variant: 'double',
+        startBy: 'bottom',
+        color,
+        overlayColor,
+      });
+
+      expect(screen.getByTestId('spinner')).toHaveStyle(startByStyles);
+    });
+
     it('Should render Spinner component with half variant', () => {
       mockPlatform('ios');
 
