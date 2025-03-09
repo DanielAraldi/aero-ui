6a321354ee1c2540003273d5b09a66df14312234
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-09 11:31:43 -0300

    test: should render `Button` component with default properties

--- packages/ui/tests/molecules/Button/index.spec.tsx
+++ packages/ui/tests/molecules/Button/index.spec.tsx
@@ -1,10 +1,43 @@
+import { colors } from '@aero-ui/tokens';
 import { render, screen } from '@testing-library/react-native';
 
 import { Button } from '../../../';
 
 describe('<Button />', () => {
-  test('Should render Button component', () => {
-    render(<Button useNativeDriver={false} />);
-    expect(screen.getByTestId('button')).toBeOnTheScreen();
+  describe('Both', () => {
+    test('Should render Button component with default properties', () => {
+      render(<Button useNativeDriver={false} />);
+
+      const button = screen.getByTestId('button');
+      const touchable = screen.getByTestId('touchable');
+      const text = screen.getByTestId('text');
+      const spinner = screen.queryByTestId('spinner');
+
+      expect(button).toHaveStyle({
+        width: '100%',
+        transform: [{ scale: 1 }],
+      });
+
+      expect(touchable).toHaveStyle({
+        backgroundColor: colors.blue[500],
+      });
+      expect(touchable).not.toHaveStyle({
+        backgroundColor: colors.blue[400],
+      });
+      expect(touchable).not.toHaveStyle({
+        backgroundColor: colors.blue[300],
+      });
+      expect(touchable).not.toBeDisabled();
+
+      expect(text).toHaveProp('children', 'Title');
+      expect(text).toHaveStyle({
+        color: colors.white[100],
+      });
+
+      expect(button).toBeOnTheScreen();
+      expect(touchable).toBeOnTheScreen();
+      expect(text).toBeOnTheScreen();
+      expect(spinner).toBeNull();
+    });
   });
 });
