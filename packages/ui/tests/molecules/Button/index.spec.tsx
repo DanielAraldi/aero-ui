86259caf90a64403e1dcc11f94061f1ed4f272a6
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-03-10 09:31:08 -0300

    test: should render `Button` component with `success` variant and change your color when its pressed

--- packages/ui/tests/molecules/Button/index.spec.tsx
+++ packages/ui/tests/molecules/Button/index.spec.tsx
@@ -119,6 +119,28 @@ describe('<Button />', () => {
       });
     });
 
+    test("Should render Button component with success variant and change your color when it's pressed", async () => {
+      render(<Button variant='success' bordered useNativeDriver={false} />);
+
+      const touchable = screen.getByTestId('touchable');
+
+      expect(touchable).toHaveStyle({
+        borderColor: colors.white[100],
+      });
+
+      fireEvent(touchable, 'pressIn');
+
+      expect(touchable).toHaveStyle({
+        borderColor: colors.green[100],
+      });
+
+      fireEvent(touchable, 'pressOut');
+
+      expect(touchable).toHaveStyle({
+        borderColor: colors.white[100],
+      });
+    });
+
     it('Should take a snapshot of the Button component', () => {
       const component = render(<Button useNativeDriver={false} />);
       expect(component).toMatchSnapshot();
