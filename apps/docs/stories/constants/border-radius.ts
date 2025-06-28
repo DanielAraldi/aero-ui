56d70497401a7b4961f90c92ed009c1172ea4d33
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-06-28 17:23:23 -0300

    docs: add `borderRadius` to select in options of the storybook

---
+++ apps/docs/stories/constants/border-radius.ts
@@ -0,0 +1,13 @@
+import { BorderRadiusKeysType } from '@aero-ui/tokens';
+
+export const borderRadius: BorderRadiusKeysType[] = [
+  'full',
+  '3xl',
+  '2xl',
+  'xl',
+  'lg',
+  'md',
+  'base',
+  'sm',
+  'none',
+];
