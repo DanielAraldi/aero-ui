84238ebdb0b12b0f986c0f0c3bfc6ecba087ddae
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-06-28 17:23:47 -0300

    refactor: remove unnecessary commands in storybook

--- apps/docs/App.tsx
+++ apps/docs/App.tsx
@@ -1,33 +1,7 @@
-import { LogBox, StyleSheet, View } from 'react-native';
-import Constants from 'expo-constants';
-import { Text } from '@aero-ui/ui';
-import { colors } from '@aero-ui/tokens';
+import { LogBox } from 'react-native';
 
 LogBox.ignoreAllLogs();
 
-function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
-
-let AppEntryPoint = App;
-
-if (Constants.expoConfig.extra.storybookEnabled === 'true') {
-  AppEntryPoint = require('./.storybook').default;
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-
-    alignItems: 'center',
-    justifyContent: 'center',
-
-    backgroundColor: colors.white[100],
-  },
-});
+let AppEntryPoint = require('./.storybook').default;
 
 export default AppEntryPoint;
